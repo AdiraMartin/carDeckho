0a15e5f96d848a1100faa94b0fc09065af3ce4e1
AdiraMartin <125848723+AdiraMartin@users.noreply.github.com>
2025-04-13 10:19:56 +0700

    app.py

--- app.py
+++ app.py
@@ -9,4 +9,32 @@ def load_data():
 
 df = load_data()
 
-st.dataframe(df)
+st.sidebar.title("Navigation")
+selected_tab = st.sidebar.radio("Go to", ["Inside Data", "Where to Sell?", "Price Prediction"])
+
+# Konten berdasarkan tab yang dipilih
+if selected_tab == "Inside Data":
+    st.title("🔍 Inside Data")
+    if selected_tab == "Inside Data":
+    st.title("🔍 Inside Data")
+
+    # Row for
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.metric(label="Total Data", value=len(df))
+
+    with col2:
+        avg_km = df["km"].mean()
+        st.metric(label="Average KM", value=f"{avg_km:,.0f} KM")
+
+    st.markdown("---")  
+
+
+elif selected_tab == "Where to Sell?":
+    st.title("📍 Where to Sell?")
+
+
+elif selected_tab == "Price Prediction":
+    st.title("💰 Price Prediction")
+
